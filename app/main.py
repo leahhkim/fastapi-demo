035df8e46ec70a2c854305241de5cbf8ee3057c6
leahkim <sxk2eh@virginia.edu>
2024-10-25 16:08:13 -0400

    added changes to yaml

--- app/main.py
+++ app/main.py
@@ -10,8 +10,15 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Leah"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
+@app.get("/square/{a}")
+def square(a: int):
+	return {"square": a*a}
+
