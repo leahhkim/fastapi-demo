c82d74ed118ad5710b61e7c0d5f9cce227aca2aa
leahkim <sxk2eh@virginia.edu>
2024-11-25 12:40:23 -0500

    added new main.py code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
+# from typing import Optional
+# from pydantic import BaseModel
 import mysql.connector
 from mysql.connector import Error
 import json
@@ -13,11 +13,7 @@ DBUSER = "ds2022"
 DBPASS = os.getenv('DBPASS')
 DB = "sxk2eh"
 
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
-
 app = FastAPI()
-
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
@@ -26,23 +22,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/genres')
-def get_genres():
-    query = "SELECT * FROM genres ORDER BY genreid;"
-    try:    
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
+@app.get("/")  # zone apex
+def zone_apex():
+    return {"Good Day": "Sunshine!"}
 
-@app.get('/songs')
+@app.get('/genres')
 async def get_genres():
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
         headers=[x[0] for x in cur.description]
@@ -55,8 +43,27 @@ async def get_genres():
         return(json_data)
     except Error as e:
         print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try:
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
         return None
-
-
-
-
